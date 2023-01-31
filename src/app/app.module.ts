7f637c8615582ab9e19cdd198a598235874bc5b8
guicalmeida <guica.almeida@gmail.com>
2023-01-31 16:13:17 -0300

    feat: create table with mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatTableModule } from '@angular/material/table';
+
 import { AppComponent } from './app.component';
 import { DataTableComponent } from './data-table/data-table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DataTableComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DataTableComponent],
+  imports: [BrowserModule, MatTableModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss'],
 })
-export class DataTableComponent {}
+export class DataTableComponent {
+  dataSource = [
+    {
+      id: 1,
+      first_name: 'Gilbertina',
+      last_name: 'Vasey',
+      email: 'gvasey0@yellowbook.com',
+      phone: '3412020199',
+      hours_studied: 132,
+      primary_group: 'Very important',
+    },
+  ];
+
+  displayedColumns: string[] = [
+    'first_name',
+    'last_name',
+    'email',
+    'phone',
+    'hours_studied',
+    'primary_group',
+  ];
+}
