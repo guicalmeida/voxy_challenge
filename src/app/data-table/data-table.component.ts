bd4e947afc333c839ae4ad98d6a672ca9569467b
guicalmeida <guica.almeida@gmail.com>
2023-01-31 16:36:12 -0300

    feat: implement dataSource to table

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,23 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentInfoApiService } from '../services/student-info-api/student-info-api.service';
+import { TableDataSourceService } from '../services/table-data-source/table-data-source.service';
 
 @Component({
   selector: 'data-table',
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.scss'],
 })
-export class DataTableComponent {
-  dataSource = [
-    {
-      id: 1,
-      first_name: 'Gilbertina',
-      last_name: 'Vasey',
-      email: 'gvasey0@yellowbook.com',
-      phone: '3412020199',
-      hours_studied: 132,
-      primary_group: 'Very important',
-    },
-  ];
-
+export class DataTableComponent implements OnInit {
   displayedColumns: string[] = [
     'first_name',
     'last_name',
@@ -26,4 +16,13 @@ export class DataTableComponent {
     'hours_studied',
     'primary_group',
   ];
+
+  dataSource!: TableDataSourceService;
+
+  constructor(private studentInfoApiService: StudentInfoApiService) {}
+
+  ngOnInit(): void {
+    this.dataSource = new TableDataSourceService(this.studentInfoApiService);
+    this.dataSource.loadStudentData();
+  }
 }
