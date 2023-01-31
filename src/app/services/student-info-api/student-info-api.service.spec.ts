a29f007891b362c7fefcba40d6824d7df6d1afdc
guicalmeida <guica.almeida@gmail.com>
2023-01-31 17:40:23 -0300

    feat: create test for basic API request

--- src/app/services/student-info-api/student-info-api.service.spec.ts
+++ src/app/services/student-info-api/student-info-api.service.spec.ts
@@ -1,16 +1,59 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { TableParams } from 'src/app/models/student-data.model';
+import { API_URL, StudentInfoApiService } from './student-info-api.service';
+import { mockTableArray } from './table-mock';
 
-import { StudentInfoApiService } from './student-info-api.service';
+/**
+ * transforms object of params in string,
+ * ignoring falsy values (except 0)
+ * @param params object of possible params
+ * @returns string of serialized params
+ */
+function serializeParams(params: TableParams) {
+  return Object.entries(params)
+    .filter((param) => param[1] || param[1] === 0)
+    .map((param) => param.join('='))
+    .flat()
+    .join('&');
+}
 
 describe('StudentInfoApiService', () => {
   let service: StudentInfoApiService;
+  let httpController: HttpTestingController;
+
+  const baseParams: TableParams = {
+    firstResult: 0,
+    maxResults: 10,
+    searchTerm: '',
+    sort: 'asc',
+    sortBy: 'first_name',
+  };
+
+  const paramsStr = serializeParams(baseParams);
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [StudentInfoApiService],
+    });
     service = TestBed.inject(StudentInfoApiService);
+    httpController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should call getStudentData and return an object with studentCount and an array of student info', (done: DoneFn) => {
+    service.getStudentData(baseParams).subscribe((res) => {
+      expect(res).toEqual(mockTableArray);
+      done();
+    });
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: `${API_URL}?${paramsStr}`,
+    });
+
+    req.flush(mockTableArray);
   });
 });
