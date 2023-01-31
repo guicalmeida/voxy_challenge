9cfd2251bbca98026c349cc6b179318402e7c489
guicalmeida <guica.almeida@gmail.com>
2023-01-31 15:41:24 -0300

    feat: add response header with cors to angular port

--- api/api.js
+++ api/api.js
@@ -4,6 +4,11 @@ const server = http.createServer(async (req, res) => {
   const parsed = url.parse(req.url, true);
 
   if (parsed.pathname === "/api" && req.method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "http://localhost:4200",
+    });
+
     res.end("hello world");
   }
 });
