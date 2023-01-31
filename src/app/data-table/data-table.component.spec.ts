525c933f337e818c078c3fc8361ef076b30abbb2
guicalmeida <guica.almeida@gmail.com>
2023-01-31 17:44:55 -0300

    feat: create test cases for table

--- src/app/data-table/data-table.component.spec.ts
+++ src/app/data-table/data-table.component.spec.ts
@@ -1,23 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatRowDef } from '@angular/material/table';
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
+import { of } from 'rxjs';
+import { AppModule } from '../app.module';
+import { StudentInfoApiService } from '../services/student-info-api/student-info-api.service';
+import { mockTableArray } from '../services/student-info-api/table-mock';
 import { DataTableComponent } from './data-table.component';
 
-describe('DataTableComponent', () => {
-  let component: DataTableComponent;
-  let fixture: ComponentFixture<DataTableComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DataTableComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(DataTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+describe('data-table component', () => {
+  beforeEach(() => {
+    return MockBuilder(DataTableComponent, AppModule).mock(
+      StudentInfoApiService,
+      { getStudentData: () => of(mockTableArray) }
+    );
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('binds dataSource', () => {
+    const targetComponent =
+      MockRender(DataTableComponent).point.componentInstance;
+
+    const tableEl = ngMocks.reveal(['mat-table']);
+
+    expect(ngMocks.input(tableEl, 'dataSource')).toBe(
+      targetComponent.dataSource
+    );
+  });
+
+  it('renders the right columns', () => {
+    const targetComponent =
+      MockRender(DataTableComponent).point.componentInstance;
+    const tableEl = ngMocks.reveal(['mat-table']);
+
+    const row = ngMocks.findInstance(tableEl, MatRowDef);
+    expect(row.columns).toBe(targetComponent.displayedColumns);
   });
 });
