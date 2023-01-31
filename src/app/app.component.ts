1279abf6cea6c461acdefa75a94aacf80cc24117
guicalmeida <guica.almeida@gmail.com>
2023-01-31 17:33:54 -0300

    fix: if table does't fit, don't show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public tableFit = false;
+  constructor() {
+    this.checkIfTableFits();
+  }
+
+  /**
+   * dynamically check if table fits view
+   */
+  @HostListener('window:resize')
+  checkIfTableFits(): void {
+    this.tableFit = window.innerWidth >= 951;
+  }
+}
