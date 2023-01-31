16242921eab734f9281af5ec6ef166a200f1b950
guicalmeida <guica.almeida@gmail.com>
2023-01-31 16:06:31 -0300

    feat: bootstrap data-table component

---
+++ src/app/data-table/data-table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss'],
+})
+export class DataTableComponent {}
